fa3cda91baaf9b2562bacdaecc50b539fde10285
Qing_LKYi <597572246@qq.com>
2022-10-26 00:56:57 +0800

    add example of readoffset

---
+++ examples/fileapi/readoffset.c
@@ -0,0 +1,48 @@
+#include <Windows.h> // 放在前面
+#include <stdio.h>
+#include <winnt.h>
+#include <winbase.h>
+#include <winternl.h>
+#include <libloaderapi.h>
+#include <minwindef.h>
+#include <fileapi.h>
+#include <handleapi.h>
+// #include <shellapi.h>
+
+typedef BOOL (WINAPI *__ReadFile) (
+    _In_ HANDLE hFile,
+    _Out_writes_bytes_to_opt_(nNumberOfBytesToRead, *lpNumberOfBytesRead) __out_data_source(FILE) LPVOID lpBuffer,
+    _In_ DWORD nNumberOfBytesToRead,
+    _Out_opt_ LPDWORD lpNumberOfBytesRead,
+    _Inout_opt_ LPOVERLAPPED lpOverlapped
+);
+
+typedef DWORD (WINAPI *__SetFilePointer) (
+    _In_ HANDLE hFile,
+    _In_ LONG lDistanceToMove,
+    _Inout_opt_ PLONG lpDistanceToMoveHigh,
+    _In_ DWORD dwMoveMethod
+);
+
+typedef BOOL (WINAPI *__CloseHandle) (
+    _In_ _Post_ptr_invalid_ HANDLE hObject
+);
+
+int main() {
+    HANDLE hf = CreateFileA(
+        "readoffset.c", 
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+    SetFilePointer(hf, 0x10, NULL, FILE_BEGIN);
+    char buf[1 << 14];
+    DWORD tmp;
+    ReadFile(hf, buf, 1 << 14, &tmp, NULL);
+    puts(buf);
+    CloseHandle(hf);
+    return 0;
+}
