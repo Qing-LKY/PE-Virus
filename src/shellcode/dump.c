e803e98c2bcba2b633b3f60cd5936542ddf57cc0
Qing_LKYi <597572246@qq.com>
2022-10-27 00:51:50 +0800

    upd string tools

--- src/shellcode/dump.c
+++ src/shellcode/dump.c
@@ -1,19 +1,44 @@
+#include <Windows.h>
+#include <winnt.h>
+#include <minwindef.h>
+
 #include <stdio.h>
 #include <string.h>
 
-const char TARGET[] = ".tiny";
+#define BUF_SIZE (1 << 20)
 
-unsigned char buf[1 << 20];
+char target[100];
+unsigned char buf[BUF_SIZE];
 
-int main() {
-    FILE *fp = fopen(TARGET, "rb");
+int main(int argc, char *argv[]) {
+    // get
+    FILE *fp;
+    if(argc == 1) {
+        scanf("%s", target);
+        fp = fopen(target, "rb");
+    } else {
+        fp = fopen(argv[1], "rb");
+    }
     fread(buf, 1, 1 << 20, fp);
-    int n = 0x26;
+    // calc
+    int n = BUF_SIZE, jmp_off = 0;
+    while(buf[n - 1] == 0) n--;
+    for(int i = 0; i < n - 4; i++) {
+        DWORD now = *(DWORD *)(buf + i);
+        if(now == 0x11223344) {
+            jmp_off = i;
+            break;
+        }
+    }
+    // output
+    printf("#define CODE_SIZE %d\n", n);
+    printf("#define JMP_POINT_OFFSET %d\n", jmp_off);
+    printf("BYTE shellCode[CODE_SIZE] = ");
     printf("{");
     for(int i = 0; i < n; i++) {
         printf("%#02x", buf[i]);
         if(i != n - 1) printf(", ");
     }
-    printf("}");
+    printf("};\n");
     return 0;
 }
--- src/shellcode/format.c
+++ src/shellcode/format.c
@@ -6,18 +6,24 @@ int main() {
     int n, i;
     scanf("%s", s);
     n = strlen(s);
-    printf("%d:\n", n);
+    printf("%d(+1: %d):\n", n, n + 1);
     printf("{");
     for(i = 0; i < n; i++) {
         printf("%#02x", (unsigned char)s[i]);
-        if(i != n - 1) printf(", ");
+        printf(", ");
     }
-    printf("}\n");
+    printf("0}\n");
     printf("{");
     for(i = 0; i < n; i++) {
         printf("L\'%c\'", s[i]);
-        if(i != n - 1) printf(", ");
+        printf(", ");
     }
-    printf("}\n");
+    printf("0}\n");
+    printf("{");
+    for(i = 0; i < n; i++) {
+        printf("\'%c\'", s[i]);
+        printf(", ");
+    }
+    printf("0}\n");
     return 0;
 }
